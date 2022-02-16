16fb7e902a16c2c01abbe6469a6979e8798f8667
Alex Chekushin <89780241+Nov1zar@users.noreply.github.com>
2022-02-16 14:31:10 +0300

    Create nav_tools.js

---
+++ nav_tools.js
@@ -0,0 +1,23 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    var inst = M.Tabs.getInstance(elems);
+
+  });
+</script>
+
+<script>
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tabs');
+  var instance = M.Tabs.init(elems, {});
+
+
+  });
+
+    
+
+  
+
+</script>
